3b8a6e487a7ac7207ca87a5f68a75e24ac7e5794
Jacob Fullerton <jfullerton@microsoft.com>
2025-12-04 17:40:40 +0000

    Base

---
+++ DayX/main.py
@@ -0,0 +1,27 @@
+"""Advent of Code 2025 - Day X"""
+
+INPUT_FILE = "/workspaces/AdventOfCode2025/DayX/input.txt"
+
+
+def solve_part1() -> int:
+    result = 0
+
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
+            line = line.strip()
+
+    return result
+
+def solve_part2() -> int:
+    result = 0
+
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
+            line = line.strip()
+
+    return result
+
+
+if __name__ == "__main__":
+    print("Part 1: " + str(solve_part1()))
+    print("Part 2: " + str(solve_part2()))
